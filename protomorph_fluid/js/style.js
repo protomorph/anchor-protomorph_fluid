354e5192fb026bf92461820719f1e80ea6be762b
protomorph <protomorph@users.noreply.github.com>
2014-10-24 14:34:03 +0100

    better readability

--- protomorph_fluid/js/style.js
+++ protomorph_fluid/js/style.js
@@ -11,18 +11,22 @@
 
 	// DROPDOWN MENU
 	// =============
+
 	$('[data-toggle="dropdown"]').on('click touchstart', function(e) {
-		$(this).parent('.dropdown').toggleClass('open');
+		$(this).parent('.dropdown')
+			.toggleClass('open');
 
 		e.preventDefault();
 	});
 
 	$('body').on('click touchstart', function(e) {
-		if (!$(e.target).is('.dropdown-menu') && !$(e.target).parents().is('.dropdown')) $('.dropdown').removeClass('open');
+		if (!$(e.target).is('.dropdown-menu') &&
+			!$(e.target).parents().is('.dropdown')) $('.dropdown').removeClass('open');
 	});
 
 	// FLOAT LABELS
 	// ============
+
 	$('html, body').find('.float-input').each(function() {
 		$(this).on('check-value', function() {
 			var _label = $(this).next('.float-label');
@@ -32,23 +36,32 @@
 			} else {
 				_label.removeClass('in');
 			}
-		}).on('keyup', function() {
+		})
+		.on('keyup', function() {
 			$(this).trigger('check-value');
-		}).on('input propertychange', function() {
+		})
+		.on('input propertychange', function() {
 			$(this).trigger('check-value');
-		}).on('textarea propertychange', function() {
+		})
+		.on('textarea propertychange', function() {
 			$(this).trigger('check-value');
-		}).trigger('check-value');
+		})
+		.trigger('check-value');
 	});
 
 	// TO TOP BUTTON
 	// =============
+
 	$('.scroll-up').on('click touchstart', function() {
-		$('html, body').animate({scrollTop: 0}, 600, 'swing');
+		$('html, body').animate({scrollTop: 0},
+			600,
+			'swing'
+		);
 	});
 
 	// TOGGLE NAVBAR
 	// =============
+
 	$('[data-toggle="collapse"]').on('click touchstart', function() {
 		$('.collapse').toggleClass('in');
 	});
